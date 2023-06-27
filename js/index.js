612357867018bf140da5c9251d56f1102e4c7e09
bettayub <bettayub19@gmail.com>
2023-06-27 12:25:46 +0300

    iterating through data items

--- js/index.js
+++ js/index.js
@@ -13,3 +13,40 @@ form.addEventListener('submit', (e) => {
   .then(data => {
     userList.innerHTML = '';
     repoList.innerHTML = '';
+
+    data.items.forEach(user => {
+        let h5 = document.createElement('h5');
+        h5.innerText = user.login;
+
+        let img = document.createElement('img');
+        img.src = user.avatar_url;
+
+        let link = document.createElement('a');
+        link.href = user.html_url;
+        link.innerText = 'Visit GitHub Account';
+
+        h5.addEventListener('click', () => {
+            fetch(`https://api.github.com/users/${user.login}/repos`)
+              .then(res => res.json())
+              .then(repos => {
+                repoList.innerHTML = '';
+                repos.forEach(repo => {
+                  let repoItem = document.createElement('li');
+                  repoItem.innerText = repo.name;
+                  repoList.appendChild(repoItem);
+                });
+              });
+          });
+  
+          let userContainer = document.createElement('div');
+          userContainer.appendChild(h5);
+          userContainer.appendChild(img);
+          userContainer.appendChild(link);
+          userList.appendChild(userContainer);
+        });
+      })
+      .catch(error => {
+        console.log('An error occurred:', error);
+      });
+
+
