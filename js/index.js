bf99f1779a530ddeb5598f0c35484fbf30b2e385
bettayub <bettayub19@gmail.com>
2023-06-27 12:26:50 +0300

    updating js

--- js/index.js
+++ js/index.js
@@ -3,18 +3,17 @@ const repoList = document.getElementById('repos-list');
 const form = document.querySelector('#github-form');
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let username = e.target.search.value;
-    console.log('Searching GitHub');
-
+  e.preventDefault();
+  let username = e.target.search.value;
+  console.log('Searching GitHub');
 
   fetch(`https://api.github.com/search/users?q=${username}`)
-  .then(res => res.json())
-  .then(data => {
-    userList.innerHTML = '';
-    repoList.innerHTML = '';
+    .then(res => res.json())
+    .then(data => {
+      userList.innerHTML = '';
+      repoList.innerHTML = '';
 
-    data.items.forEach(user => {
+      data.items.forEach(user => {
         let h5 = document.createElement('h5');
         h5.innerText = user.login;
 
@@ -26,27 +25,26 @@ form.addEventListener('submit', (e) => {
         link.innerText = 'Visit GitHub Account';
 
         h5.addEventListener('click', () => {
-            fetch(`https://api.github.com/users/${user.login}/repos`)
-              .then(res => res.json())
-              .then(repos => {
-                repoList.innerHTML = '';
-                repos.forEach(repo => {
-                  let repoItem = document.createElement('li');
-                  repoItem.innerText = repo.name;
-                  repoList.appendChild(repoItem);
-                });
+          fetch(`https://api.github.com/users/${user.login}/repos`)
+            .then(res => res.json())
+            .then(repos => {
+              repoList.innerHTML = '';
+              repos.forEach(repo => {
+                let repoItem = document.createElement('li');
+                repoItem.innerText = repo.name;
+                repoList.appendChild(repoItem);
               });
-          });
-  
-          let userContainer = document.createElement('div');
-          userContainer.appendChild(h5);
-          userContainer.appendChild(img);
-          userContainer.appendChild(link);
-          userList.appendChild(userContainer);
+            });
         });
-      })
-      .catch(error => {
-        console.log('An error occurred:', error);
+
+        let userContainer = document.createElement('div');
+        userContainer.appendChild(h5);
+        userContainer.appendChild(img);
+        userContainer.appendChild(link);
+        userList.appendChild(userContainer);
       });
-
-
+    })
+    .catch(error => {
+      console.log('An error occurred:', error);
+    });
+});
