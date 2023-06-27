327929e9f28ca6b919697d6af4cbb713c752add1
bettayub <bettayub19@gmail.com>
2023-06-27 12:21:07 +0300

    adding event listeners

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
 const userList = document.getElementById('user-list');
 const repoList = document.getElementById('repos-list');
 const form = document.querySelector('#github-form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let username = e.target.search.value;
+    console.log('Searching GitHub');
